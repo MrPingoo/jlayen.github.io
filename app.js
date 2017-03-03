433b2961aea278116c6a3c8361376d573203d97d
Julian Layen <Julian@MacBook-Pro-de-Julian.local>
2017-03-03 13:48:32 +0100

    Debug #1

--- app.js
+++ app.js
@@ -2,8 +2,24 @@
 
 var app = angular.module('demo', ['ngRoute', 'hc.marked']);
 
+
+var app = angular.module('demo', ['ngRoute', 'hc.marked'], function($httpProvider){
+    if (!!window.FastClick) {
+        FastClick.attach(document.body);
+    }
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+}).run(['$location', function($location){
+    //Allows us to navigate to the correct element on initialization
+    if ($location.path() !== '' && $location.path() !== '/') {
+        smoothScroll(document.getElementById($location.path().substring(1)), 500, function(el) {
+            location.replace('#' + el.id);
+        });
+    }
+}]);
+
 app.config([
     '$interpolateProvider', function($interpolateProvider) {
         return $interpolateProvider.startSymbol('{(').endSymbol(')}');
     }
 ]);
+
