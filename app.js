e0adb978bfc7eed53495719a5eec90e5cc5870ef
Julian Layen <Julian@MacBook-Pro-de-Julian.local>
2017-03-06 00:00:44 +0100

    Article 5 (1/2)

--- app.js
+++ app.js
@@ -14,12 +14,14 @@ var app = angular.module('app', ['ngRoute', 'hc.marked'], function($httpProvider
     }
 }]);
 
-/* For production mode */
+/* For production mode*/
 app.config([
     '$interpolateProvider', function($interpolateProvider) {
         return $interpolateProvider.startSymbol('{(').endSymbol(')}');
     }
 ]);
+
+
 
 app.config(['markedProvider', function (markedProvider) {
     markedProvider.setOptions({
